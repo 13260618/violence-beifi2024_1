2a86e8b3a14fe2eb4033df679d04b700fd6823f3
Israel Monjaraz <111941844+13260618@users.noreply.github.com>
2023-11-28 23:56:47 -0600

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,73 +1,26 @@
 library(shiny)
 library(shinythemes)
+library(ggplot2)
+library(plotly)
+
+
+library(maps)
+#library(maptools)
 
 
 
-navbarPage(
-  theme = shinytheme("simplex"), 
-  collapsible = TRUE,
-  id = "nav",
-  windowTitle = "WOMEN",
-  HTML('<a style="text-decoration:none;cursor:default" class="active" href="#">WOMEN-2023</a>'),
+
+bootstrapPage(
   
-  tabPanel("Resultados",
-           sidebarLayout(
-             sidebarPanel(
-               width = 3,
-               fluidRow(
-                 column(
-                   width = 12,
-                   sliderInput("bins_tab1",
-                               "Número de Bins:",
-                               min = 1,
-                               max = 50,
-                               value = 30),
-                   numericInput('numb',"numero de graphs",
-                                min=4,
-                                max=19, value = 7)
-                 )
-               )
-             ),
-             mainPanel(
-               width = 9,
-               fluidRow(
-                 column(
-                   width = 6,
-                   plotOutput("histogram_tab1")
-                 ),
-                 column(
-                   width = 6,
-                   verbatimTextOutput("text_output")  
-                 )
-               )
-             )
-           )
-  ),
-  
-  tabPanel("Muertes",
-           sidebarLayout(
-             sidebarPanel(
-               # Input: Slider for the number of bins (for Tab 2)
-               sliderInput("bins_tab2",
-                           "Número de Bins (Tab 2):",
-                           min = 1,
-                           max = 50,
-                           value = 30)
-             ),
-             mainPanel(
-               plotOutput("histogram_tab2")
-             )
-           )
-  ),
   tags$head(
-  tags$style(
-  HTML("
+    tags$style(
+      HTML("
     .navbar-nav { /* Hace las pestañas verticales */
       flex-direction: column !important;
     }
     
      body {
-        background-color: #ffffff; /* Cambia el color de fondo a blanco */
+        background-color:background-color: rgba(255, 255, 255, 0.4); /* Cambia el color de fondo a blanco */
      }
       
     .sidebar {
@@ -90,7 +43,135 @@ navbarPage(
     .navbar-nav .active { /* Cambia el color de fondo de la pestaña activa */
       background-color: rgba(235, 130, 235, 0.4);
     }
+    
+    
+    
+    
+    /* Estilos para la línea de separación */
+      .separation-line {
+        border-left: 2px solid #3498db; /* Color de la línea (azul) */
+        padding-left: 10px; /* Espaciado izquierdo para la línea */
+      }
+      
+       /* Estilos personalizados para el tabsetPanel */
+      .nav-tabs .nav-link {
+        border-bottom: 2px solid #3498db; /* Línea de borde inferior en las pestañas */
+        background-color: rgba(255, 255, 255, 0.4);
+      }
+    
+    
+    
   "
-       ))
-)
-)
+           )
+      )
+    ),
+  
+
+  
+  navbarPage(
+    theme = shinytheme("simplex"), 
+    collapsible = TRUE,
+    id = "nav",
+    windowTitle = "WOMEN",
+    HTML('<a style="text-decoration:none;cursor:default" class="active" href="#">Violence-2023</a>'),
+    
+    
+    
+    
+    tabPanel("Resultados",
+             column(6,
+                    tabsetPanel(
+                      id = "tabs",
+                      tabPanel("Tab 1", 
+                               #Contenido de la pestaña 
+                               
+                               fluidRow(
+                                 column(
+                                   width = 6,
+                                   sliderInput("bins_tab1",
+                                               "Número de Bins:",
+                                               min = 1,
+                                               max = 50,
+                                               value = 30),
+                                   
+                                   dateInput("f0", HTML("<b style=color:#045a8d>Tiempo de inicio (ti)</b>"), 
+                                             value = as.Date("2021/05/28"), 
+                                             format = "yyyy/mm/dd", 
+                                            #min = min(data0$Fecha), max = max(data0$Fecha)
+                                   )
+                                 ),
+                                   
+                                   #plotOutput("plot1")
+                                 )
+                               )
+                      ),
+                      # tabPanel("Tab 2", 
+                      #          # Contenido de la pestaña 2
+                      #          fluidRow(
+                      #            column(
+                      #              width = 6,
+                      #              plotOutput("plot2")
+                      #            )
+                      #          )
+                      # ),
+                      # tabPanel("Tab 3", 
+                      #          # Contenido de la pestaña 3
+                      #          fluidRow(
+                      #            column(
+                      #              width = 6,
+                      #              plotOutput("plot3")
+                      #            )
+                      #          )
+                      # )
+                    ),
+             
+             
+             column(6,
+                    mainPanel( 
+                      plotlyOutput("histogram")
+                    )
+                    
+                    
+                    
+                    )
+             
+             
+             )
+    ,
+    
+    tabPanel(
+      "Regionalización",
+      column(6,
+             
+             )),
+      
+      tabPanel(
+        "Aspectos",
+        column(6,
+               
+        )),
+        
+        tabPanel(
+          "Feminicidios",
+          column(6,
+                 
+          )),
+          
+          tabPanel(
+            "Históricos",
+            column(6,
+                   
+            ))
+      
+      
+    )
+  )
+    
+    
+    
+    
+    
+    
+      
+                                     
+  
