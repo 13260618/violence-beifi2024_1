d0f16f8716b8514cb34f6b186456ac92672412f5
Israel Monjaraz <111941844+13260618@users.noreply.github.com>
2023-11-29 11:30:51 -0600

    Update server.R

--- server.R
+++ server.R
@@ -1,68 +1,105 @@
- 
+
 
 library(shiny)
+library(plotly)
+
+
 #hombres y mujeres en mex 2021
-m = 65500000
-h = 128000000 - m
+tot = 126014024 
+m = 51.2
+h = 48.8
 #mujeres mas y menos de 15  
 m15 = m*0.771
 mno15 = m - m15
 
 function(input, output) { 
   # 
-  # Generar histograma para la pestaña Tab 1
-  output$histogram <- renderPlotly({
-    x <- rnorm(500, mean = 0, sd = 1)
+  # Generar histograma para la pestaña Tab 1 
+  lista_graficos <- list(
+    plot_ly(x = c("Mujeres", "Hombres", "Psicológica", "Física", "Sexual", "Discri"), 
+            y = c(60, 40, NA, NA, NA,NA),
+            type = 'bar', 
+            name = "Población total",
+            marker = list(color = 'rgba(162, 51, 255, 0.5)')
+    ) %>%
+      add_trace(y = c( 20, 30,50,45,NA, NA ),
+                name = "Violencia en mujeres",
+                marker = list(color = '#a68df4')
+      ) %>%
+      layout(title = 'Distribución porcentual de mujeres en México (2021)',
+             plot_bgcolor='#ffffff', 
+             xaxis = list(zerolinecolor = '#888',
+                          zerolinewidth = 1, 
+                          gridcolor = '#ddd'), 
+             yaxis = list(zerolinecolor = '#888',
+                          zerolinewidth = 1, 
+                          gridcolor = '#ddd')
+      )
+  )
+  
+  
+  
+  output$plot_01 <- renderPlotly({
+    lista_graficos[[1]]
     
-      if (!is.null(input$bins_tab1) && !is.na(input$bins_tab1) && input$bins_tab1 > 0) {
-      bins <- seq(min(x), max(x), length.out = input$bins_tab1 + 1)
-      plot_ly(x = x, type = "histogram", histnorm = "probability", 
-              autobinx = FALSE,
-              xbins = list(start = min(bins),
-                           end = max(bins), 
-                           size = (max(bins)-min(bins))/input$bins_tab1)) %>%
-        layout(title = "Histograma - Tab 1",
-               xaxis = list(title = "Valores"),
-               yaxis = list(title = "Frecuencia"))
-    } else {
-      plot_ly(x = NULL, type = "scatter", mode = "markers") %>%
-        layout(title = "Datos insuficientes para el histograma")
-    }
+    
+    
+    # if (input$button_1 == 1) {
+    #   lista_graficos[[1]]
+    # } else if (input$button_2 ==2) {
+    #   lista_graficos[[2]]
+    # } else {
+    #   lista_graficos[[1]]  # Muestra el primer gráfico por defecto al iniciar
+    # }
+    
+    
+    
+    # fig <-  plot_ly(x = c("Mujeres","Hombres"), y = c(m,h), type = 'bar') %>%
+    #   layout(title = 'Proporción de mujeres en México (2021)',
+    #          plot_bgcolor='#e5ecf6', 
+    #          xaxis = list( 
+    #            zerolinecolor = '#ffff', 
+    #            zerolinewidth = 2, 
+    #            gridcolor = 'ffff'), 
+    #          yaxis = list( 
+    #            zerolinecolor = '#ffff', 
+    #            zerolinewidth = 2, 
+    #            gridcolor = 'ffff'))
+    # 
+    # fig
+    
   })
-  
-  output$text_output <- renderText({
-    if (input$caso == "Porcentaje de mujeres en el país (2021)") {
-      # Lógica para la interpretación del primer caso
-      return("Aquí va la interpretación del primer caso...")
-    } else if (input$caso == "Encuestas de violencia 2016 vs 2021") {
-      # Lógica para la interpretación del segundo caso
-      return("Aquí va la interpretación del segundo caso...")
-    } else {
-      return(NULL)
-    }
-  })
-  
-  
-
+ 
   output$text_01 <- renderText({
     if (input$caso == "Porcentaje de mujeres en el país (2021)") {
-    HTML("
+      HTML("
     De acuerdo a las dos encuestas del INEGI realizadas en los años 2016 y 2021,
     las mujeres mayores a 15 años son víctimas de violencia de género en un 70.1%, 
     es decir de un total de 65.5 millones de mujeres a dicha fecha aproximadamente 
     46 millones.
     ")
     }
-   else if (input$caso == "Encuestas de violencia 2016 vs 2021") {
+    else if (input$caso == "Encuestas de violencia 2016 vs 2021") {
       HTML("
       La violencia psicológica es la más afecta a las mujeres ya  que está
          presente en 51.6%,  seguida de la violencia sexual (49.7 %), la 
          violencia física (34.7 %) y la violencia económica, patrimonial y/o
          discriminación (27.4 %).
   ")
-  }
-  else {
-    return(NULL)
-  }
+    }
+    else {
+      return(NULL)
+    }
   })
 }
+
+
+
+
+ 
+
+
+
+
+
+
