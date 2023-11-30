ef1e593ea39e41b7a0df7951018ede6b6a20ef25
Israel Monjaraz <111941844+13260618@users.noreply.github.com>
2023-11-29 18:40:26 -0600

    Update server.R

--- server.R
+++ server.R
@@ -4,72 +4,65 @@ library(shiny)
 library(plotly)
 
 
-#hombres y mujeres en mex 2021
-tot = 126014024 
-m = 51.2
-h = 48.8
-#mujeres mas y menos de 15  
-m15 = m*0.771
-mno15 = m - m15
+# Definición de datos
+tot <- 126014024 
+m <- 51.2
+h <- 48.8
+mh.df <- data.frame(
+  x = c('Mujeres', 'Hombres'),
+  y = c(m, h),
+  text = c("128 M","60 M")
+)
 
-function(input, output) { 
-  # 
-  # Generar histograma para la pestaña Tab 1 
-  lista_graficos <- list(
-    plot_ly(x = c("Mujeres", "Hombres", "Psicológica", "Física", "Sexual", "Discri"), 
-            y = c(60, 40, NA, NA, NA,NA),
-            type = 'bar', 
-            name = "Población total",
-            marker = list(color = 'rgba(162, 51, 255, 0.5)')
-    ) %>%
-      add_trace(y = c( 20, 30,50,45,NA, NA ),
-                name = "Violencia en mujeres",
-                marker = list(color = '#a68df4')
-      ) %>%
-      layout(title = 'Distribución porcentual de mujeres en México (2021)',
-             plot_bgcolor='#ffffff', 
-             xaxis = list(zerolinecolor = '#888',
-                          zerolinewidth = 1, 
-                          gridcolor = '#ddd'), 
-             yaxis = list(zerolinecolor = '#888',
-                          zerolinewidth = 1, 
-                          gridcolor = '#ddd')
-      )
-  )
-  
-  
-  
-  output$plot_01 <- renderPlotly({
-    lista_graficos[[1]]
-    
-    
-    
-    # if (input$button_1 == 1) {
-    #   lista_graficos[[1]]
-    # } else if (input$button_2 ==2) {
-    #   lista_graficos[[2]]
-    # } else {
-    #   lista_graficos[[1]]  # Muestra el primer gráfico por defecto al iniciar
-    # }
-    
-    
-    
-    # fig <-  plot_ly(x = c("Mujeres","Hombres"), y = c(m,h), type = 'bar') %>%
-    #   layout(title = 'Proporción de mujeres en México (2021)',
-    #          plot_bgcolor='#e5ecf6', 
-    #          xaxis = list( 
-    #            zerolinecolor = '#ffff', 
-    #            zerolinewidth = 2, 
-    #            gridcolor = 'ffff'), 
-    #          yaxis = list( 
-    #            zerolinecolor = '#ffff', 
-    #            zerolinewidth = 2, 
-    #            gridcolor = 'ffff'))
-    # 
-    # fig
-    
-  })
+order_categories <- c("Discriminación", "Física", "Psicológica", "Sexual", "Hombres", "Mujeres")
+
+tipo_viole.df <- data.frame(
+  x = factor(c("Psicológica", "Física", "Sexual", "Discriminación"), 
+             levels = order_categories[1:4]),
+  y = c(51.6, 49.7, 34.7, 27.4),
+  text = c("68.8 M", "30 M", "15 M", "12 M")
+)
+
+# Definición de la app Shiny
  
+  server = function(input, output) { 
+    output$plot_01 <- renderPlotly({
+      mh.plt <- plot_ly(mh.df, 
+                        x = ~x, 
+                        y = ~y,
+                        type = 'bar',
+                        name = 'Población total',
+                        text = ~text,
+                        marker = list(color = 'purple', 
+                                      opacity = 0.5)
+                        
+                        )
+      
+      tipo_viole.plt <- mh.plt %>% add_trace(data = tipo_viole.df, 
+                                             x = ~x, 
+                                             y = ~y,
+                                             type = 'bar', 
+                                             name = 'Tipos de violencia',
+                                             text = ~text,
+                                             marker = list(color = 'red',
+                                                           opacity = 0.5))
+      
+      mh.plt <- tipo_viole.plt %>% layout(
+        title = 'Distribución porcentual de mujeres violentadas',
+        xaxis = list(title = ' ', 
+                     categoryorder = "array", 
+                     categoryarray = order_categories),
+        yaxis = list(title = 'Porcentaje')
+      )
+      
+      # Mostrar el gráfico combinado
+      mh.plt
+    })
+  
+  
+  
+  
+  
   output$text_01 <- renderText({
     if (input$caso == "Porcentaje de mujeres en el país (2021)") {
       HTML("
@@ -92,14 +85,5 @@ function(input, output) {
     }
   })
 }
-
-
-
-
- 
-
-
-
-
 
 
